91bd3fe3f129a1cce963633143ab89cc8a8de0c7
Igor Ruzhilov <iruzhilov@gmail.com>
2023-07-03 10:10:52 +0600

    Fix db_config file

--- iitu_site/core/settings.py
+++ iitu_site/core/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from django.templatetags import static
 
 from .db_config import DB_NAME, DB_PASS, DB_HOST, DB_USER, DB_PORT
+from .db_config import DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
