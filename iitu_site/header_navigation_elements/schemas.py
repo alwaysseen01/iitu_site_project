83e3e6eb43979ae3c64f356249365e8114c3e24d
Igor Ruzhilov <iruzhilov@gmail.com>
2023-07-04 11:58:50 +0600

    Add CRUD API for Dropdown elements (Categories: 1, 2, 3)

--- iitu_site/header_navigation_elements/schemas.py
+++ iitu_site/header_navigation_elements/schemas.py
@@ -3,8 +3,47 @@ from ninja import Schema
 
 class NavigationElementIn(Schema):
     title: str
+    is_dropdown: bool
 
 
 class NavigationElementOut(Schema):
     id: int
     title: str
+    is_dropdown: bool
+
+
+class DropdownNavigationElement1_In(Schema):
+    title: str
+    parent_navigation_element: int
+    is_dropdown: bool
+
+
+class DropdownNavigationElement1_Out(Schema):
+    id: int
+    title: str
+    parent_navigation_element: int
+    is_dropdown: bool
+
+
+class DropdownNavigationElement2_In(Schema):
+    title: str
+    parent_navigation_element: int
+    is_dropdown: bool
+
+
+class DropdownNavigationElement2_Out(Schema):
+    id: int
+    title: str
+    parent_navigation_element: int
+    is_dropdown: bool
+
+
+class DropdownNavigationElement3_In(Schema):
+    title: str
+    parent_navigation_element: int
+
+
+class DropdownNavigationElement3_Out(Schema):
+    id: int
+    title: str
+    parent_navigation_element: int
--- iitu_site/header_navigation_elements/views.py
+++ iitu_site/header_navigation_elements/views.py
@@ -6,8 +6,11 @@ from django.core.exceptions import ObjectDoesNotExist
 from ninja import Router
 from ninja.errors import HttpError
 
-from .models import NavigationElement
-from .schemas import NavigationElementIn, NavigationElementOut
+from .models import NavigationElement, DropdownNavigationElement1, DropdownNavigationElement2, \
+    DropdownNavigationElement3
+from .schemas import NavigationElementIn, NavigationElementOut, DropdownNavigationElement1_In, \
+    DropdownNavigationElement1_Out, DropdownNavigationElement2_Out, DropdownNavigationElement2_In, \
+    DropdownNavigationElement3_Out, DropdownNavigationElement3_In
 
 # Create your views here.
 router = Router(tags=["Header navigation elements"])
@@ -17,6 +20,7 @@ router = Router(tags=["Header navigation elements"])
 async def create_navigation_element(request, data_in: NavigationElementIn):
     navigation_element = await sync_to_async(NavigationElement.objects.create)(
         title=data_in.title,
+        is_dropdown=data_in.is_dropdown,
     )
     return {"id": navigation_element.id}
 
@@ -26,6 +30,7 @@ async def update_navigation_element(request, navigation_element_id: int, data_in
     try:
         navigation_element = await sync_to_async(NavigationElement.objects.get)(id=navigation_element_id)
         navigation_element.title = data_in.title
+        navigation_element.is_dropdown = data_in.is_dropdown
         await sync_to_async(navigation_element.save)()
         return {"success": True}
     except ObjectDoesNotExist:
@@ -49,3 +54,126 @@ async def list_navigation_elements(request):
         return [NavigationElementOut.from_orm(element) for element in navigation_elements]
     except ObjectDoesNotExist:
         raise HttpError(404, "Header navigation elements not found")
+
+
+@router.post("/")
+async def create_dropdown_1_element(request, data_in: DropdownNavigationElement1_In):
+    dropdown_navigation_element = await sync_to_async(DropdownNavigationElement1.objects.create)(
+        title=data_in.title,
+        parent_navigation_element=data_in.parent_navigation_element,
+        is_dropdown=data_in.is_dropdown,
+    )
+    return {"id": dropdown_navigation_element.id}
+
+
+@router.put("/{dropdown_1_element_id}/")
+async def update_dropdown_1_element(request, dropdown_1_element_id: int, data_in: DropdownNavigationElement1_In):
+    try:
+        dropdown_navigation_element = await sync_to_async(DropdownNavigationElement1.objects.get)(id=dropdown_1_element_id)
+        dropdown_navigation_element.title = data_in.title
+        dropdown_navigation_element.parent_navigation_element = data_in.parent_navigation_element
+        dropdown_navigation_element.is_dropdown = data_in.is_dropdown
+        await sync_to_async(dropdown_navigation_element.save)()
+        return {"success": True}
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation element not found")
+
+
+@router.delete("/{dropdown_1_element_id}/")
+async def delete_dropdown_1_element(request, dropdown_1_element_id: int):
+    try:
+        dropdown_navigation_element = await sync_to_async(DropdownNavigationElement1.objects.get)(id=dropdown_1_element_id)
+        await sync_to_async(dropdown_navigation_element.delete)()
+        return {"success": True}
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation element not found")
+
+
+@router.get("/")
+async def list_dropdown_1_elements(request):
+    try:
+        dropdown_navigation_elements = await sync_to_async(list)(DropdownNavigationElement1.objects.all())
+        return [DropdownNavigationElement1_Out.from_orm(element) for element in dropdown_navigation_elements]
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation elements not found")
+
+@router.post("/")
+async def create_dropdown_2_element(request, data_in: DropdownNavigationElement2_In):
+    dropdown_navigation_element = await sync_to_async(DropdownNavigationElement2.objects.create)(
+        title=data_in.title,
+        parent_navigation_element=data_in.parent_navigation_element,
+        is_dropdown=data_in.is_dropdown,
+    )
+    return {"id": dropdown_navigation_element.id}
+
+
+@router.put("/{dropdown_2_element_id}/")
+async def update_dropdown_2_element(request, dropdown_2_element_id: int, data_in: DropdownNavigationElement2_In):
+    try:
+        dropdown_navigation_element = await sync_to_async(DropdownNavigationElement2.objects.get)(id=dropdown_2_element_id)
+        dropdown_navigation_element.title = data_in.title
+        dropdown_navigation_element.parent_navigation_element = data_in.parent_navigation_element
+        dropdown_navigation_element.is_dropdown = data_in.is_dropdown
+        await sync_to_async(dropdown_navigation_element.save)()
+        return {"success": True}
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation element not found")
+
+
+@router.delete("/{dropdown_2_element_id}/")
+async def delete_dropdown_2_element(request, dropdown_2_element_id: int):
+    try:
+        dropdown_navigation_element = await sync_to_async(DropdownNavigationElement2.objects.get)(id=dropdown_2_element_id)
+        await sync_to_async(dropdown_navigation_element.delete)()
+        return {"success": True}
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation element not found")
+
+
+@router.get("/")
+async def list_dropdown_2_elements(request):
+    try:
+        dropdown_navigation_elements = await sync_to_async(list)(DropdownNavigationElement2.objects.all())
+        return [DropdownNavigationElement2_Out.from_orm(element) for element in dropdown_navigation_elements]
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation elements not found")
+
+
+@router.post("/")
+async def create_dropdown_3_element(request, data_in: DropdownNavigationElement3_In):
+    dropdown_navigation_element = await sync_to_async(DropdownNavigationElement3.objects.create)(
+        title=data_in.title,
+        parent_navigation_element=data_in.parent_navigation_element,
+    )
+    return {"id": dropdown_navigation_element.id}
+
+
+@router.put("/{dropdown_3_element_id}/")
+async def update_dropdown_3_element(request, dropdown_3_element_id: int, data_in: DropdownNavigationElement3_In):
+    try:
+        dropdown_navigation_element = await sync_to_async(DropdownNavigationElement3.objects.get)(id=dropdown_3_element_id)
+        dropdown_navigation_element.title = data_in.title
+        dropdown_navigation_element.parent_navigation_element = data_in.parent_navigation_element
+        await sync_to_async(dropdown_navigation_element.save)()
+        return {"success": True}
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation element not found")
+
+
+@router.delete("/{dropdown_3_element_id}/")
+async def delete_dropdown_3_element(request, dropdown_3_element_id: int):
+    try:
+        dropdown_navigation_element = await sync_to_async(DropdownNavigationElement3.objects.get)(id=dropdown_3_element_id)
+        await sync_to_async(dropdown_navigation_element.delete)()
+        return {"success": True}
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation element not found")
+
+
+@router.get("/")
+async def list_dropdown_3_elements(request):
+    try:
+        dropdown_navigation_elements = await sync_to_async(list)(DropdownNavigationElement3.objects.all())
+        return [DropdownNavigationElement3_Out.from_orm(element) for element in dropdown_navigation_elements]
+    except ObjectDoesNotExist:
+        raise HttpError(404, "Header dropdown navigation elements not found")
