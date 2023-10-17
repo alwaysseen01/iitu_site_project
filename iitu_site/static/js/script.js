d9b655a3a79f8f45f68101296b30496ecdd4db66
a <voharko@gmail.com>
2023-10-17 18:31:15 +0600

    scrollable buttons

--- iitu_site/static/js/script.js
+++ iitu_site/static/js/script.js
@@ -18,3 +18,36 @@ function openCity(evt, eduName) {
     document.getElementById(eduName).style.display = "grid";
     evt.currentTarget.className += " active";
 }
+
+
+function setupScrollButtons() {
+    // Получите ссылку на ваш горизонтальный блок
+    var outerBlock = document.getElementsByClassName('advantages-cardbox')[0];
+  
+    // Получите ссылки на ваши кнопки
+    var leftButton = document.getElementById('leftButton');
+    var rightButton = document.getElementById('rightButton');
+  
+    // Получите все внутренние блоки по классу
+    var innerBlocks = document.getElementsByClassName('advantages-card');
+  
+    // Добавьте обработчики событий для ваших кнопок
+    leftButton.addEventListener('click', function() {
+      // Прокрутите влево на ширину первого внутреннего блока
+      outerBlock.scrollTo({
+        left: outerBlock.scrollLeft - innerBlocks[0].offsetWidth,
+        behavior: 'smooth'
+      });
+    });
+  
+    rightButton.addEventListener('click', function() {
+      // Прокрутите вправо на ширину первого внутреннего блока
+      outerBlock.scrollTo({
+        left: outerBlock.scrollLeft + innerBlocks[0].offsetWidth,
+        behavior: 'smooth'
+      });
+    });
+}
+  
+// Вызовите функцию, чтобы настроить кнопки прокрутки
+setupScrollButtons();
